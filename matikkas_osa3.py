a5b7676ddde6b378c1dc08626676c97c4e111b26
teemu <teemu.kallio3@metropolia.fi>
2023-09-11 01:56:16 +0300

    osa3

---
+++ matikkas_osa3.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+A = np.array([[-1, 2], [3, -5]])
+B = np.array([[2, 0], [-1, 4]])
+
+print(2*A+3*B)
+print(A-B)
+
+A2 = np.array([[1+1j, 3, 1j], [0, 5+1j, -1j]])
+print((1+1j)*A2)
+
+
